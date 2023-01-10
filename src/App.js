6505e07667fa83479305b13cb8c36acc793c2b94
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-10 11:58:19 +0000

    got the response from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import AllItems from './Components/Items/AllItems';
 import Menu from './Components/Menu/Menu';
@@ -11,7 +11,8 @@ function App() {
       <BrowserRouter>
         <Menu />
         <Routes>
-          <Route path="/AllItems" element={<AllItems />}></Route>
+          <Route path="*" element={<Navigate to="/AllItems" replace />} />
+          <Route path="/AllItems" element={<AllItems />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Items/AllItems.jsx
+++ src/Components/Items/AllItems.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 const AllItems = () => {
+  useEffect(() => {
+    axios
+      .get('https://localhost:7172/api/Items/0,10', {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        },
+        responseType: 'json',
+      })
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
   return (
     <div>
       <h1>AllItems</h1>
