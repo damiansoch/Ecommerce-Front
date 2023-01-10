0d0b3c64c0190253720773552925827f29e55514
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-10 11:21:37 +0000

    first route completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import AllItems from './Components/Items/AllItems';
 import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
-      <AllItems />
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/AllItems" element={<AllItems />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -2,16 +2,18 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import { Link } from 'react-router-dom';
+
 const Menu = () => {
   return (
     <>
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">SellerApp</Navbar.Brand>
+          <Navbar.Brand>SellerApp</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">All Items</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link to="/AllItems">All Items</Link>
+            <Link to="">Features</Link>
+            <Link to="">Pricing</Link>
           </Nav>
         </Container>
       </Navbar>
